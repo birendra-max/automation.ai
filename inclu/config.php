faeabb4d56c658bf90ee6bd689b7220d248ee02d
Birendra <birendrapradhan112@gmail.com>
2025-03-23 17:42:53 +1000

    Change comgif.php

--- inclu/config.php
+++ inclu/config.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = new mysqli('localhost', 'bravodent_design', 'h.UWqR@Ayaeht5DD', 'automation.ai') or die('Connection Error');
+$conn = new mysqli('localhost', 'root', '', 'automation.ai') or die('Connection Error');
