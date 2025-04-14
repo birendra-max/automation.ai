598a6420a70a73c2457802f701d7ef9bb1cd12ae
Birendra <birendrapradhan112@gmail.com>
2025-04-15 02:03:54 +0530

    Change code base

--- inclu/config.php
+++ inclu/config.php
@@ -1,3 +1,5 @@
 <?php
 
 $conn = new mysqli('localhost', 'root', '', 'automation.ai') or die('Connection Error');
+
+// $conn = new mysqli('localhost', 'u450172623_ai', 'R~:vnQx7:', 'u450172623_automation') or die('Connection Error');
