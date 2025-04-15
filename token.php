3e5f6a598825aa23a9663354a3de139eb9fbcb78
Birendra <birendrapradhan112@gmail.com>
2025-04-16 01:52:34 +0530

    Change the code

--- token.php
+++ token.php
@@ -15,10 +15,7 @@ use Twilio\Jwt\Grants\VoiceGrant;
 $identity = $_SESSION['user_details']['role'] ?? 'anonymous';
 
 // ✅ Use the actual strings (not getenv)
-$accountSid     = 'AC4ecb1ba7f942506281dbafac19ddd398';
-$apiKeySid      = 'SKa2ae7ea7201ddfeeaff4bf3cb66201c9';
-$apiKeySecret   = 'NP70531hpRPOnBpvKZGrCFRUezw65saO';
-$twimlAppSid    = 'AP18b523b07ca9758eacc1d731c9c5ef7f';
+
 
 // Generate the token
 $token = new AccessToken(
